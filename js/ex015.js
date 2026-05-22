d4769f1d373d54ff7251baf37bfaebd0fcf978b9
Pedro Mascarenhas <pedromascarenhas100@.com>
2026-05-21 21:38:21 -0300

    Adicionando arquivos em JS

---
+++ js/ex015.js
@@ -0,0 +1,7 @@
+
+let i = 0
+
+while (i< 100){
+    console.log(`Comer a ${i} fatia de pizza`)
+    i = i+1
+}
---
+++ js/ex017.js
@@ -0,0 +1,31 @@
+let nomeAluno ="Pedro"
+let idadeAluno= 30
+let cursoAluno ="Desenvolvimento"
+let periodoAluno="Noite"
+
+
+
+const aluno = {
+    nome: "Carlos",
+    idade: 20,
+    curso:
+    {
+        nomeCurso: "Desenvolvimento",
+        dataTermino: "09/06/2026",
+        professor: "Jefferson"
+    },
+    periodo:"Noturno"
+}
+
+//console.log(aluno)
+//console.log(aluno.nome)
+//console.log(aluno.periodo)
+//console.log(aluno.curso.nomeCurso)
+//console.log(aluno.curso.dataTermino)
+
+
+let arr=[1,2,3,4,5]
+let frutas=["Maça", "Banana","Uva"]
+
+console.log(frutas)
+console.log(frutas[2])
