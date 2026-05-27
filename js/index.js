7c17d09ab621f297b2a1b69425a00205713e5242
Pedro Mascarenhas <pedromascarenhas100gmail.com>
2026-05-27 19:04:29 -0300

    Add arquivos JS

---
+++ js/index.js
@@ -0,0 +1,11 @@
+console.log("Executado pelo arquivo index.js")
+
+function somarNumeros(x,y){
+    console.log("Aqui é feito a soma dos números")
+    // let n1= 5
+    // let n2= 3
+   let soma = x+y
+   console.log(soma)
+}
+
+somarNumeros(20,20)
